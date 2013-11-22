3f62fe8293bd49f57e779b90b0a5bea862ecf194
anigam <anigam@nd.edu>
2013-11-22 06:28:28 -0500

    Version 0.1 OS and Scheduler classes

--- DEvSim/src/system/OS.java
+++ DEvSim/src/system/OS.java
@@ -1,23 +1,61 @@
 package system;
 
-import java.util.Queue;
+import java.util.*;
 
+import system.Scheduler;
 import process.Event;
-import resources.Block;
+import resources.*;
+import process.Process;
+
 
 public class OS {
 
-	static Queue<Event> eventQueue;
+	private static Queue<Event> eventQueue;
+	Scheduler scheduler; 
+
 	public static int TIME;
+	int quantum;
+	int context_switch_cost;
 
-	public static void main(String[] args) {
-		
+	//Initialize the OS
+	public OS()
+	{
+		TIME=0;
+		quantum=5;
+		context_switch_cost=1;
 	}
 	
-	public void iterate() {
-		
+	public void startOS(Vector<Process> ProcessList)
+	{
+		//Add Processes to os
+		scheduler.addProcess(ProcessList);
 	}
 	
-	
-
+	public void runOS()
+	{
+		//add first event to queue
+		Event curEvent=scheduler.getCurrentEvent();
+		eventQueue.add(curEvent);
+		
+		while(!eventQueue.isEmpty())
+		{
+			Event e=eventQueue.remove();
+			ResourceManager manager;
+			int page_fault_count = manager.needs(e.getResourcesNeeded());
+			if(page_fault_count==0) //Event has all the desired resources
+			{
+				int remTime=quantum;
+				while(remTime>0)
+				{
+					remTime=e.DoEvent(quantum);
+				}
+				
+			}
+			boolean check=scheduler.contextSwitch();
+			if(check)
+				OS.TIME += context_switch_cost;
+			
+				OS.TIME += quantum;			
+		}
+	}	
 }
--- DEvSim/src/system/Scheduler.java
+++ DEvSim/src/system/Scheduler.java
@@ -1,5 +1,38 @@
 package system;
 
+import java.util.Queue;
+import java.util.Vector;
+
+import process.Event;
+import process.Process;
+
 public class Scheduler {
 
+	private Vector<Process> ProcessList;
+	
+	public void addProcess(Vector<Process> ProcessList)
+	{
+		this.ProcessList=ProcessList;
+	}
+	
+	public Event getCurrentEvent()
+	{
+		Event e=null;
+		Process curProcess=ProcessList.elementAt(0);
+		e=curProcess.PopEvent();
+		return e;
+	}
+	
+	public boolean contextSwitch()
+	{
+		 Process curProcess = ProcessList.remove(0);
+		 if (curProcess.get_remaining_event_num()>0)
+		 {
+			 ProcessList.add(curProcess);
+			 return true;
+		 }
+		 if(ProcessList.isEmpty())
+			 return false;
+		 return false;
+	}
 }
